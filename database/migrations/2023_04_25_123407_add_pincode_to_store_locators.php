c0c673b3a67f1ca1d17eacce534d22ef4701f980
duraipixel <durairaj.pixel@gmail.com>
2023-04-25 18:26:46 +0530

    migrate file changed

--- database/migrations/2023_04_25_123407_add_pincode_to_store_locators.php
+++ database/migrations/2023_04_25_123407_add_pincode_to_store_locators.php
@@ -14,7 +14,7 @@ class AddPincodeToStoreLocators extends Migration
     public function up()
     {
         Schema::table('store_locators', function (Blueprint $table) {
-            //
+            $table->string('pincode')->nullable()->after('longitude');
         });
     }
 
@@ -26,7 +26,7 @@ class AddPincodeToStoreLocators extends Migration
     public function down()
     {
         Schema::table('store_locators', function (Blueprint $table) {
-            //
+            $table->dropColumn('pincode');
         });
     }
 }
