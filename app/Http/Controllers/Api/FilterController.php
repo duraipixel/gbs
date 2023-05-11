872f1bc967e14ec77ba2f4c3ea8cd0abc8c476a9
duraipixel <durairaj.pixel@gmail.com>
2023-05-11 13:13:58 +0530

    space removed inslug

--- app/Http/Controllers/Api/FilterController.php
+++ app/Http/Controllers/Api/FilterController.php
@@ -542,8 +542,8 @@ class FilterController extends Controller
                     $sub_array = [];
                     foreach ($sub_values as $items_sub ) {
                         $temp_val = [];
-                        $temp_val['name'] = $items_sub->attribute_values;                    
-                        $temp_val['slug'] = str_replace(' ', '__', $items_sub->attribute_values);   
+                        $temp_val['name'] = trim($items_sub->attribute_values);                    
+                        $temp_val['slug'] = str_replace(' ', '__', trim($items_sub->attribute_values));   
                         $sub_array[] = $temp_val;                 
                     }
                     $attributes[str_replace(' ', '_', strtolower($att_value->title))] = $sub_array;
