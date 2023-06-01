eea0310b05e4ec19d2fc91aacde3c36ef817d24a
duraipixel <durairaj.pixel@gmail.com>
2023-06-01 13:00:04 +0530

    test6

--- resources/views/payment/request_handler.blade.php
+++ resources/views/payment/request_handler.blade.php
@@ -23,7 +23,7 @@
         <form method="post" name="redirect"
             action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
             
-            <input type=hidden name="enc_request" value="<?= $encrypted_data ?>">
+            <input type=hidden name="encRequest" value="<?= $encrypted_data ?>">
             <input type=hidden name="access_code" value="<?= $access_code ?>">
             ?>
         </form>
