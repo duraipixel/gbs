1115006491307b2e341f721a16f3b4d047166fae
duraipixel <durairaj.pixel@gmail.com>
2023-06-01 13:06:58 +0530

    tseting9

--- resources/views/payment/request_handler.blade.php
+++ resources/views/payment/request_handler.blade.php
@@ -6,26 +6,25 @@
 
 <body>
     <center>
+        @php
+            $merchant_data = '2';
+            $working_key = 'B00B81683DCD0816F8F32551E2C2910B'; //Shared by CCAVENUES
+            $access_code = 'AVRD71KE07CJ75DRJC'; //Shared by CCAVENUES
+            
+            foreach ($_POST as $key => $value) {
+                $merchant_data .= $key . '=' . $value . '&';
+            }
+            
+            $encrypted_data = encrypt($merchant_data, $working_key); // Method for encrypting the data.
+        @endphp
 
-        <?php
-        
-        $merchant_data = '2';
-        $working_key = 'B00B81683DCD0816F8F32551E2C2910B'; //Shared by CCAVENUES
-        $access_code = 'AVRD71KE07CJ75DRJC'; //Shared by CCAVENUES
-        
-        foreach ($_POST as $key => $value) {
-            $merchant_data .= $key . '=' . $value . '&';
-        }
-        
-        $encrypted_data = encrypt($merchant_data, $working_key); // Method for encrypting the data.
-        
-        ?>
+       
         <form method="post" name="redirect"
             action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
+
+            <input type=hidden name="encRequest" value="{{ $encrypted_data  }}">
+            <input type=hidden name="access_code" value="{{ $access_code }}">
             
-            <input type=hidden name="encRequest" value="<?= $encrypted_data ?>">
-            <input type=hidden name="access_code" value="<?= $access_code ?>">
-            ?>
         </form>
     </center>
     <script language='javascript'>
