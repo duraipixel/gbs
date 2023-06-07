e2b46bab9209474c8a29c0f385fc8e157f9e94d7
duraipixel <durairaj.pixel@gmail.com>
2023-06-07 16:32:30 +0530

    testeing89

--- app/Http/Controllers/Payment/CCavenueController.php
+++ app/Http/Controllers/Payment/CCavenueController.php
@@ -548,7 +548,7 @@ class CCavenueController extends Controller
         curl_setopt($ch, CURLOPT_URL, "https://apitest.ccavenue.com/apis/servlet/DoWebTrans");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: application/json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $final_data);
