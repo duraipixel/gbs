0b423d29dfad33595746ad3549145a8dcb67b8cc
duraipixel <durairaj.pixel@gmail.com>
2023-06-16 10:42:34 +0530

    bug fixing1

--- app/Http/Controllers/Payment/CCavenueController.php
+++ app/Http/Controllers/Payment/CCavenueController.php
@@ -263,7 +263,7 @@ class CCavenueController extends Controller
         $shippping_fee_amount = 0;
 
         $cart_info = Cart::selectRaw('sum(sub_total) as total, coupon_id, coupon_amount, shipping_fee_id, shipping_fee')->where('customer_id', $customer_id)->first();
-        $cart_addon_info = Cart::selectRaw('cart_product_addons(amount) as addon_total, coupon_id, coupon_amount, shipping_fee_id, shipping_fee')
+        $cart_addon_info = Cart::selectRaw('gbs_cart_product_addons(amount) as addon_total, coupon_id, coupon_amount, shipping_fee_id, shipping_fee')
                             ->leftJoin('cart_product_addons', 'cart_product_addons.product_id', '=', 'carts.product_id')->where('customer_id', $customer_id)->first();
         dump( $cart_info );
         dd( $cart_addon_info );
