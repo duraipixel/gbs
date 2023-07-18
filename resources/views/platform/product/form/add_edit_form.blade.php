73991151134e619197e3ad0743177841052e8167
duraipixel <durairaj.pixel@gmail.com>
2023-07-18 12:01:05 +0530

    screen size aadded for api

--- resources/views/platform/product/form/add_edit_form.blade.php
+++ resources/views/platform/product/form/add_edit_form.blade.php
@@ -261,25 +261,6 @@
 
                     }
                 }
-
-                // this.on("addedfile", function(file) {
-                //     // Create the remove button
-                //     var removeButton = Dropzone.createElement("<button>Remove file</button>");
-                //     // Capture the Dropzone instance as closure.
-                //     var _this = this;
-                //     // Listen to the click event
-                //     removeButton.addEventListener("click", function(e) {
-                //         // Make sure the button click doesn't submit the form:
-                //         e.preventDefault();
-                //         e.stopPropagation();
-                //         // Remove the file preview.
-                //         _this.removeFile(file);
-                //         // If you want to the delete the file on the server as well,
-                //         // you can do the AJAX request here.
-                //     });
-                //     // Add the button to the file preview element.
-                //     file.previewElement.appendChild(removeButton);
-                // });
               
             },
             removedfile: function (file) {
