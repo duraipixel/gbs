67c3f4930797f145442ddd61ccdb5fd477db9fe3
duraipixel <durairaj.pixel@gmail.com>
2023-08-03 12:42:25 +0530

    test 2

--- mailer/send-mail.php
+++ mailer/send-mail.php
@@ -5,49 +5,44 @@ error_reporting(E_ALL);
 
 require_once 'phpmailer/PHPMailerAutoload.php';
 
-function send_mail($tomail, $mlsubject, $bdymsg, $for, $storename)
-{
-
-    // $_REQUEST[]; 
-
-    $to = 'prabhu.k.pixel@gmail.com';
-
-    $subject = $mlsubject;
-
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->isHTML(true);
-    $mail->Host = 'mail.gbssystems.com';
-    $mail->Port       = 587;
-    $mail->SMTPSecure = 'tls';
-    $mail->SMTPAuth   = true;
-    $mail->Username = 'support@gbssystems.com';
-    $mail->Password = 'GHltfCmPf3O';
-    $mail->From = 'support@gbssystems.com';
-    $mail->FromName = 'GBS Customer Care';
-    $mail->addAddress($to);
-    $mail->Subject = $subject;
-    $mail->msgHTML($bdymsg);
 
 
+// $_REQUEST[]; 
 
-    $mail->SMTPDebug = 3;
-    $mail->Debugoutput = 'html';
+$to = 'prabhu.k.pixel@gmail.com';
+
+$subject = $mlsubject;
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->isHTML(true);
+$mail->Host = 'mail.gbssystems.com';
+$mail->Port       = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth   = true;
+$mail->Username = 'support@gbssystems.com';
+$mail->Password = 'GHltfCmPf3O';
+$mail->From = 'support@gbssystems.com';
+$mail->FromName = 'GBS Customer Care';
+$mail->addAddress($to);
+$mail->Subject = $subject;
+$mail->msgHTML($bdymsg);
 
 
 
-    if (!$mail->send()) {
-        return [
-            "status" => false,
-            "message" => "Mailer Error: " . $mail->ErrorInfo
-        ];
-    }    
-    
+$mail->SMTPDebug = 3;
+$mail->Debugoutput = 'html';
+
+
+
+if (!$mail->send()) {
     return [
-        "status" => true,
-        "message" => "Mail send Success!"
+        "status" => false,
+        "message" => "Mailer Error: " . $mail->ErrorInfo
     ];
-
 }
 
-echo send_mail('pravin@pixel-studios.com', 'SMTP Mail function', 'Test mail content', '1', 'GBS');
+return [
+    "status" => true,
+    "message" => "Mail send Success!"
+];
