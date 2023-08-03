f57a2c3d202e2409f7489b6aa5de00b2e6e05fcd
duraipixel <durairaj.pixel@gmail.com>
2023-08-03 12:47:01 +0530

    test 4

--- mailer/send-mail.php
+++ mailer/send-mail.php
@@ -5,43 +5,44 @@ error_reporting(E_ALL);
 
 require_once 'phpmailer/PHPMailerAutoload.php';
 
+function send_mail($to, $subject, $bdymsg, $for, $storename)
+{
 
 
-// $_REQUEST[]; 
-
-$addAddress = 'durairaj.pixel@gmail.com';
-$Subject    = "Test mail from DEVTEAM";
-$msgHTML    = "<h1>Hello </h1>";
-
-
-
-$mail       = new PHPMailer;
-$mail->isSMTP();
-$mail->isHTML(true);
-$mail->Host       = 'mail.gbssystems.com';
-$mail->Port       = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth   = true;
-$mail->Username   = 'support@gbssystems.com';
-$mail->Password   = 'GHltfCmPf3O';
-$mail->From       = 'support@gbssystems.com';
-$mail->FromName   = 'GBS Customer Care';
-$mail->addAddress($addAddress);
-$mail->Subject = $Subject;
-$mail->msgHTML($msgHTML);
-$mail->SMTPDebug   = 3;
-$mail->Debugoutput = 'html';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->isHTML(true);
+    $mail->Host = 'mail.gbssystems.com';
+    $mail->Port       = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth   = true;
+    $mail->Username = 'support@gbssystems.com';
+    $mail->Password = 'GHltfCmPf3O';
+    $mail->From = 'support@gbssystems.com';
+    $mail->FromName = 'GBS Customer Care';
+    $mail->addAddress($to);
+    $mail->Subject = $subject;
+    $mail->msgHTML($bdymsg);
 
 
 
-if (!$mail->send()) {
+    $mail->SMTPDebug = 3;
+    $mail->Debugoutput = 'html';
+
+
+
+    if (!$mail->send()) {
+        return [
+            "status" => false,
+            "message" => "Mailer Error: " . $mail->ErrorInfo
+        ];
+    }    
+    
     return [
-        "status" => false,
-        "message" => "Mailer Error: " . $mail->ErrorInfo
+        "status" => true,
+        "message" => "Mail send Success!"
     ];
+
 }
 
-return [
-    "status" => true,
-    "message" => "Mail send Success!"
-];
+echo send_mail('durairaj.pixel@gmail.com', 'SMTP Mail function', 'Test mail content', '1', 'GBS');
