b502d86e488a8584b1f536094ce03c6a240c0d5c
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-28 16:11:11 +0530

    FIX  : Cart List API Update

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -254,144 +254,149 @@ class CartController extends Controller
         $total_addon_amount = 0;
         $has_pickup_store = true;
         $brand_array = [];
-        if (isset($checkCart) && !empty($checkCart)) {
-            foreach ($checkCart as $citems) {
-                $used_addons = [];
-                $items = $citems->products;
-                $tax = [];
-                $tax_percentage = 0;
-
-                $category               = $items->productCategory;
-                $price_with_tax         = $items->mrp;
-                if (isset($category->parent->tax_id) && !empty($category->parent->tax_id)) {
-                    $tax_info = Tax::find($category->parent->tax_id);
-                } else if (isset($category->tax_id) && !empty($category->tax_id)) {
-                    $tax_info = Tax::find($category->tax_id);
-                }
-                // dump( $citems );
-                if (isset($tax_info) && !empty($tax_info)) {
-                    $tax = getAmountExclusiveTax($price_with_tax, $tax_info->pecentage);
-                    $tax_total =  $tax_total + ($tax['gstAmount'] * $citems->quantity) ?? 0;
-                    $product_tax_exclusive_total = $product_tax_exclusive_total + ($tax['basePrice'] * $citems->quantity);
-                    // print_r( $product_tax_exclusive_total );
-                    $tax_percentage         = $tax['tax_percentage'] ?? 0;
-                } else {
-                    $product_tax_exclusive_total = $product_tax_exclusive_total + $citems->sub_total;
-                }
-
-                /**
-                 * addon amount calculated here
-                 */
-                $addonItems = CartProductAddon::where(['cart_id' => $citems->id, 'product_id' => $items->id ])->get();
-                
-                $addon_total = 0;
-                if( isset( $addonItems ) && !empty( $addonItems ) && count($addonItems) > 0 ) {
-                    foreach ($addonItems as $addItems) {
-                        if( isset( $addItems->addonItem->addon ) && !empty( $addItems->addonItem->addon ) ) {
-
-                            $addons = [];
-                            $addons['addon_id'] = $addItems->addonItem->addon->id;
-                            $addons['title'] = $addItems->addonItem->addon->title;
-                            $addons['description'] = $addItems->addonItem->addon->description;
+        
+        try {
+            if (isset($checkCart) && !empty($checkCart)) {
+                foreach ($checkCart as $citems) {
+                    $used_addons = [];
+                    $items = $citems->products;
+                    $tax = [];
+                    $tax_percentage = 0;
     
-                            if (!Storage::exists( $addItems->addonItem->addon->icon)) {
-                                $path               = asset('assets/logo/no_Image.jpg');
-                            } else {
-                                $url                = Storage::url( $addItems->addonItem->addon->icon);
-                                $path               = asset($url);
-                            }
-                            $addons['addon_item_id'] = $addItems->addonItem->id;
-                            $addons['icon'] = $path;
-                            $addons['addon_item_label'] = $addItems->addonItem->label;
-                            $addons['amount'] = $addItems->addonItem->amount;
-                            $addon_total += $addItems->addonItem->amount;
-                            $used_addons[] = $addons;
-                        }
-
+                    $category               = $items->productCategory;
+                    $price_with_tax         = $items->mrp;
+                    if (isset($category->parent->tax_id) && !empty($category->parent->tax_id)) {
+                        $tax_info = Tax::find($category->parent->tax_id);
+                    } else if (isset($category->tax_id) && !empty($category->tax_id)) {
+                        $tax_info = Tax::find($category->tax_id);
                     }
+                    // dump( $citems );
+                    if (isset($tax_info) && !empty($tax_info)) {
+                        $tax = getAmountExclusiveTax($price_with_tax, $tax_info->pecentage);
+                        $tax_total =  $tax_total + ($tax['gstAmount'] * $citems->quantity) ?? 0;
+                        $product_tax_exclusive_total = $product_tax_exclusive_total + ($tax['basePrice'] * $citems->quantity);
+                        // print_r( $product_tax_exclusive_total );
+                        $tax_percentage         = $tax['tax_percentage'] ?? 0;
+                    } else {
+                        $product_tax_exclusive_total = $product_tax_exclusive_total + $citems->sub_total;
+                    }
+    
+                    /**
+                     * addon amount calculated here
+                     */
+                    $addonItems = CartProductAddon::where(['cart_id' => $citems->id, 'product_id' => $items->id ])->get();
+                    
+                    $addon_total = 0;
+                    if( isset( $addonItems ) && !empty( $addonItems ) && count($addonItems) > 0 ) {
+                        foreach ($addonItems as $addItems) {
+                            if( isset( $addItems->addonItem->addon ) && !empty( $addItems->addonItem->addon ) ) {
+    
+                                $addons = [];
+                                $addons['addon_id'] = $addItems->addonItem->addon->id;
+                                $addons['title'] = $addItems->addonItem->addon->title;
+                                $addons['description'] = $addItems->addonItem->addon->description;
+        
+                                if (!Storage::exists( $addItems->addonItem->addon->icon)) {
+                                    $path               = asset('assets/logo/no_Image.jpg');
+                                } else {
+                                    $url                = Storage::url( $addItems->addonItem->addon->icon);
+                                    $path               = asset($url);
+                                }
+                                $addons['addon_item_id'] = $addItems->addonItem->id;
+                                $addons['icon'] = $path;
+                                $addons['addon_item_label'] = $addItems->addonItem->label;
+                                $addons['amount'] = $addItems->addonItem->amount;
+                                $addon_total += $addItems->addonItem->amount;
+                                $used_addons[] = $addons;
+                            }
+    
+                        }
+                    }
+    
+                    $total_addon_amount += $addon_total;
+                  
+                    $pro                    = [];
+                    $pro['id']              = $items->id;
+                    $pro['tax']             = $tax;
+                    $pro['tax_percentage']  = $tax_percentage;
+                    $pro['hsn_no']          = $items->hsn_code ?? null;
+                    $pro['product_name']    = $items->product_name;
+                    $pro['category_name']   = $category->name ?? '';
+                    $pro['brand_name']      = $items->productBrand->brand_name ?? '';
+                    $pro['hsn_code']        = $items->hsn_code;
+                    $pro['product_url']     = $items->product_url;
+                    $pro['sku']             = $items->sku;
+                    $pro['stock_status']    = $items->stock_status;
+                    $pro['is_featured']     = $items->is_featured;
+                    $pro['is_best_selling'] = $items->is_best_selling;
+                    $pro['price']           = $items->mrp;
+                    $pro['strike_price']    = $items->strike_price;
+                    $pro['save_price']      = $items->strike_price - $items->mrp;
+                    $pro['discount_percentage'] = abs($items->discount_percentage);
+                    $pro['image']           = $items->base_image;
+                    $pro['max_quantity']    = $items->quantity;
+                    $imagePath              = $items->base_image;
+    
+                    $brand_array[] = $items->brand_id;
+    
+                    if (!Storage::exists($imagePath)) {
+                        $path               = asset('assets/logo/no_Image.jpg');
+                    } else {
+                        $url                = Storage::url($imagePath);
+                        $path               = asset($url);
+                    }
+    
+                    $pro['image']           = $path;
+                    $pro['customer_id']     = $customer_id;
+                    $pro['guest_token']     = $citems->guest_token;
+                    $pro['cart_id']         = $citems->id;
+                    $pro['price']           = $citems->price;
+                    $pro['quantity']        = $citems->quantity;
+                    $pro['sub_total']       = $citems->sub_total;
+                    $pro['addons']          = $used_addons;
+                    $grand_total            += $citems->sub_total;
+                    $grand_total            += $addon_total;
+                    $cartTemp[] = $pro;
+                    
                 }
-
-                $total_addon_amount += $addon_total;
-              
-                $pro                    = [];
-                $pro['id']              = $items->id;
-                $pro['tax']             = $tax;
-                $pro['tax_percentage']  = $tax_percentage;
-                $pro['hsn_no']          = $items->hsn_code ?? null;
-                $pro['product_name']    = $items->product_name;
-                $pro['category_name']   = $category->name ?? '';
-                $pro['brand_name']      = $items->productBrand->brand_name ?? '';
-                $pro['hsn_code']        = $items->hsn_code;
-                $pro['product_url']     = $items->product_url;
-                $pro['sku']             = $items->sku;
-                $pro['stock_status']    = $items->stock_status;
-                $pro['is_featured']     = $items->is_featured;
-                $pro['is_best_selling'] = $items->is_best_selling;
-                $pro['price']           = $items->mrp;
-                $pro['strike_price']    = $items->strike_price;
-                $pro['save_price']      = $items->strike_price - $items->mrp;
-                $pro['discount_percentage'] = abs($items->discount_percentage);
-                $pro['image']           = $items->base_image;
-                $pro['max_quantity']    = $items->quantity;
-                $imagePath              = $items->base_image;
-
-                $brand_array[] = $items->brand_id;
-
-                if (!Storage::exists($imagePath)) {
-                    $path               = asset('assets/logo/no_Image.jpg');
-                } else {
-                    $url                = Storage::url($imagePath);
-                    $path               = asset($url);
+    
+                $tmp['carts'] = $cartTemp;
+                $tmp['cart_count'] = count($cartTemp);
+                if (isset($shipping_info) && !empty($shipping_info) || (isset( $selected_shipping ) && !empty( $selected_shipping )) ) {
+                    $tmp['selected_shipping_fees'] = array(
+                                                    'shipping_id' => $shipping_info->id ?? $selected_shipping['shipping_id'],
+                                                    'shipping_charge_order' => $shipping_info->charges ?? $selected_shipping['shipping_charge_order'],
+                                                    'shipping_type' => $shipping_type ?? $selected_shipping['shipping_type'] ?? 'fees'
+                                                    );
+                    
+                    $grand_total                = $grand_total + ($shipping_info->charges ?? $selected_shipping['shipping_charge_order'] ?? 0);
                 }
-
-                $pro['image']           = $path;
-                $pro['customer_id']     = $customer_id;
-                $pro['guest_token']     = $citems->guest_token;
-                $pro['cart_id']         = $citems->id;
-                $pro['price']           = $citems->price;
-                $pro['quantity']        = $citems->quantity;
-                $pro['sub_total']       = $citems->sub_total;
-                $pro['addons']          = $used_addons;
-                $grand_total            += $citems->sub_total;
-                $grand_total            += $addon_total;
-                $cartTemp[] = $pro;
-                
+                if( isset( $coupon_data ) && !empty( $coupon_data ) ) {
+                    $grand_total = $grand_total - $coupon_data['discount_amount'] ?? 0;
+                }
+    
+                if( count(array_unique($brand_array)) > 1 ) {
+                    $has_pickup_store = false;
+                } 
+    
+                $amount         = filter_var($grand_total, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                $charges        = ShippingCharge::select('id', 'shipping_title', 'minimum_order_amount', 'charges', 'is_free')->where('status', 'published')->where('minimum_order_amount', '<', $amount)->get();
+    
+                $tmp['shipping_charges']    = $charges;
+                $tmp['cart_total']          = array(
+                    'total' => number_format(round($grand_total), 2),
+                    'product_tax_exclusive_total' => number_format(round($product_tax_exclusive_total), 2),
+                    'product_tax_exclusive_total_without_format' => round($product_tax_exclusive_total),
+                    'tax_total' => number_format(round($tax_total), 2),
+                    'tax_percentage' => number_format(round($tax_percentage), 2),
+                    'shipping_charge' => $shipping_info->charges ?? 0,
+                    'addon_amount' => $total_addon_amount,
+                    'has_pickup_store' => $has_pickup_store,
+                    'brand_id' => $brand_array[0] ?? ''
+                );
             }
-
-            $tmp['carts'] = $cartTemp;
-            $tmp['cart_count'] = count($cartTemp);
-            if (isset($shipping_info) && !empty($shipping_info) || (isset( $selected_shipping ) && !empty( $selected_shipping )) ) {
-                $tmp['selected_shipping_fees'] = array(
-                                                'shipping_id' => $shipping_info->id ?? $selected_shipping['shipping_id'],
-                                                'shipping_charge_order' => $shipping_info->charges ?? $selected_shipping['shipping_charge_order'],
-                                                'shipping_type' => $shipping_type ?? $selected_shipping['shipping_type'] ?? 'fees'
-                                                );
-                
-                $grand_total                = $grand_total + ($shipping_info->charges ?? $selected_shipping['shipping_charge_order'] ?? 0);
-            }
-            if( isset( $coupon_data ) && !empty( $coupon_data ) ) {
-                $grand_total = $grand_total - $coupon_data['discount_amount'] ?? 0;
-            }
-
-            if( count(array_unique($brand_array)) > 1 ) {
-                $has_pickup_store = false;
-            } 
-
-            $amount         = filter_var($grand_total, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            $charges        = ShippingCharge::select('id', 'shipping_title', 'minimum_order_amount', 'charges', 'is_free')->where('status', 'published')->where('minimum_order_amount', '<', $amount)->get();
-
-            $tmp['shipping_charges']    = $charges;
-            $tmp['cart_total']          = array(
-                'total' => number_format(round($grand_total), 2),
-                'product_tax_exclusive_total' => number_format(round($product_tax_exclusive_total), 2),
-                'product_tax_exclusive_total_without_format' => round($product_tax_exclusive_total),
-                'tax_total' => number_format(round($tax_total), 2),
-                'tax_percentage' => number_format(round($tax_percentage), 2),
-                'shipping_charge' => $shipping_info->charges ?? 0,
-                'addon_amount' => $total_addon_amount,
-                'has_pickup_store' => $has_pickup_store,
-                'brand_id' => $brand_array[0] ?? ''
-            );
+        } catch (\Throwable $th) {
+            //throw $th;
         }
         
         return $tmp;
